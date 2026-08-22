eac2b585700f658a576f6305ecce2ce41eea46c2
Credkellar-Boop <darnellwashingtonjr94@gmail.com>
2026-08-21 19:04:44 -0700

    Update sandb0x-xtract0r.rb

--- Formula/sandb0x-xtract0r.rb
+++ Formula/sandb0x-xtract0r.rb
@@ -2,7 +2,7 @@ class Sandb0xXtract0r < Formula
   desc "Automated cross-platform security analysis engine for malicious binaries"
   homepage "https://github.com/darnellwashingtonjr94-art/SandB0x-Xtract0r"
   url "https://github.com/darnellwashingtonjr94-art/SandB0x-Xtract0r/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "379ba233f819a1728d4434079148a6e36c6ffe77e937d0d10d83f5ba120ea6b"
+  sha256 "379ba233f819a1728d4434079148a6e0636cfe77e937d0d10d83f5ba120ea6b"
   license "MIT"
 
   depends_on "rust" => :build
